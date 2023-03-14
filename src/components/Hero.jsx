fceb1b0242649ef3d22beaac6827351c5c8b33d5
Juan Gustin <juandgustin@gmail.com>
2023-03-14 18:48:05 -0500

    CLICK TEXT

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,16 +26,16 @@ const Hero = () => {
 
           <br />
           <p className={styles.sectionSubText}>
-            (Pro Tip: Arrastra el escritorio a los lados)
+            (Pro Tip: Puedes mover el escritorio.)
           </p>
         </div>
       </div>
       <ComputersCanvas />
       {/* Moble Tocuh Button */}
-      <div className="absolute bottom-20 md:bottom-10 lg:bottom-10 w-full flex justify-center items-center">
+      <div className="absolute bottom-20 md:bottom-10 lg:bottom-10 w-full flex flex-col justify-center items-center">
         {/* Moble */}
         <a href="#about">
-          <div className=" w-[30px] h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className=" w-[30px] h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 border-slate-50 hover:border-slate-300">
             <motion.div
               animate={{
                 y: [0, 15, 0],
@@ -45,9 +45,16 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="w-2 h-2 rounded-full bg-secondary mb-1"
+              className={`w-2 h-2 rounded-full bg-slate-50 mb-4 hover:bg-slate-300`}
             />
           </div>
+        </a>
+        <a href="#about">
+          <p
+            className={`${styles.sectionSubText} pt-4 text-gray-50 hover:text-stone-300`}
+          >
+            Clickeame para bajar
+          </p>
         </a>
       </div>
     </section>
