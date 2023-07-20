9a254bf4c9f31f16aef26559ddb61214d437ac78
Juan Gustin <juandgustin@gmail.com>
2023-07-20 12:55:22 -0500

    Deploy Fix 8: Vite Config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/developer-portfolio/',
+  base: '/',
   build: {
     chunkSizeWarningLimit: 1000,
   },
