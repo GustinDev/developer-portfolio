fb38f91f43cb242588cee7c5ca12162407261d01
Juan Gustin <juandgustin@gmail.com>
2023-07-20 12:18:37 -0500

    Deploy Fix 2: Images Fixed 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/developer-portfolio/",
+  base: '/developer-portfolio/',
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 });
