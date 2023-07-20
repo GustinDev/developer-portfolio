287105f019aa0f19851185bf4312bd372ecbe008
Juan Gustin <juandgustin@gmail.com>
2023-07-20 12:35:37 -0500

    Deploy Fix 5: Basename Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import {
   About,
   Contact,
@@ -10,14 +10,14 @@ import {
   Tech,
   Works,
   StarsCanvas,
-} from "./components";
-import Personal from "./components/Personal";
+} from './components';
+import Personal from './components/Personal';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+    <BrowserRouter basename='/'>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
@@ -27,7 +27,7 @@ const App = () => {
         <Feedbacks />
         <Experience />
         <Personal />
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
